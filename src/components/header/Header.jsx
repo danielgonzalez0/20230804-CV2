733278dd942b3a80488dda1008b2b63bf107fb53
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2023-11-06 14:38:30 +0100

    update: header and sidebar change

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const text = `En reconversion, je suis aujourd'hui à la recherche d'un premier poste développeur frontend junior. 
-Professionnel doté d'une grande expérience en contrôle de gestion, je suis capable de fonctionner de manière autonome, et fais preuve d'un fort sens de l'organisation et de capacités d’analyse avérées. 
-De nature empathique, je suis à l’écoute des autres et je peux facilement travailler en équipe.
+const text = `Développeur frontend spécialisé en React, je suis capable de travailler sur des projets d'intégration web, d'optimisation ou de débogage de code, ou encore d'implémentation de nouvelles fonctionnalités.
+Ancien contrôleur de gestion, j'ai pu acquérir de nombreuses connaissances financières et comptables sur la gestion d'entreprise. 
 `;
+// const text = `En reconversion, je suis aujourd'hui à la recherche d'un premier poste développeur frontend junior. 
+// Professionnel doté d'une grande expérience en contrôle de gestion, je suis capable de fonctionner de manière autonome, et fais preuve d'un fort sens de l'organisation et de capacités d’analyse avérées. 
+// De nature empathique, je suis à l’écoute des autres et je peux facilement travailler en équipe.
+// `;
 
 const Header = () => {
   return (
