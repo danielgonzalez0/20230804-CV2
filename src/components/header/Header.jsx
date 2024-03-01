a41c0603f46a8cece4f576125c81f76ca951857a
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-03-01 20:15:16 +0100

    fix cv text error

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const text = `Professionnel aguerri, je m’investis depuis 2 ans dans le développement web. Rigueur, autonomie, fiabilité et engagement sont autant de valeurs que je porte dans chaque projet. Pour moi, le métier de développeur représente une opportunité de partager des connaissances, d'apprendre de nouvelles technologies et de collaborer d'un objectif commun.`;
+const text = `Professionnel aguerri, je m’investis depuis 2 ans dans le développement web. Rigueur, autonomie, fiabilité et engagement sont autant de valeurs que je porte dans chaque projet. Pour moi, le métier de développeur représente une opportunité de partager des connaissances, d'apprendre de nouvelles technologies et de collaborer autour d'un objectif commun.`;
 // const text = `Développeur web spécialisé en React, je suis capable de travailler sur des projets d'intégration web, d'optimisation ou de débogage de code, ou encore d'implémentation de nouvelles fonctionnalités.
 // Ancien contrôleur de gestion, j'ai pu acquérir de nombreuses connaissances financières et comptables sur la gestion d'entreprise. 
 // `;
